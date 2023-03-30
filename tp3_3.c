9ea43600bab9b08ba337b78bd3ffd63e3191011f
Inaki <inakipoch106@gmail.com>
2023-03-30 18:38:46 -0300

    Generated strings

--- tp3_3.c
+++ tp3_3.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 
 int main(int argc,char** argv) {
+    char** names = (char**)malloc(sizeof(char*)*5);
+    char* buffer = (char*)malloc(100);
+    for(int i = 0; i < 5; i++) {
+        printf("\nIngresar la frase %d: ",i+1);
+        gets(buffer);
+        names[i] = (char*)malloc(strlen(buffer)+1);
+        strcpy(names[i],buffer);
+    }
+    for(int i = 0; i < 5; i ++) {
+        printf("Frase %d: %s\n",i+1,names[i]);
+    }
+    free(names);
+    free(buffer);
     return 0;
 }
