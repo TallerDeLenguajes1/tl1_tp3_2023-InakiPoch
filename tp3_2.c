4c791e7bfef329653ea4453ce24a2ed2bd487a22
Inaki <inakipoch106@gmail.com>
2023-03-30 17:58:41 -0300

    Added average and minMaxValues functions

--- tp3_2.c
+++ tp3_2.c
@@ -2,8 +2,43 @@
 #include <stdlib.h>
 #include <time.h>
 
+void averageRevenue(int production[5][12]) {
+    for(int i = 0 ; i < 5 ; i++) {
+        int average = 0;
+        for(int j = 0 ; j < 12 ; j++) {
+            average += production[i][j];
+        }
+        printf("Promedio del anio %d: %d\n",i+1,average/12);
+    }
+}
+
+void maxAndMin(int production[5][12]) {
+    int maxValue = production[0][0];
+    int minValue = production[0][0];
+    int maxYear = 0, maxMonth = 0;
+    int minYear = 0, minMonth = 0;
+    for(int i = 0 ; i < 5 ; i++) {
+        for(int j = 0 ; j < 12 ; j++) {
+            if(production[i][j] > maxValue) {
+                maxValue = production[i][j];
+                maxYear = i + 1;
+                maxMonth = j + 1;
+            }
+            else {
+                if(production[i][j] < minValue) {
+                    minValue = production[i][j];
+                    minYear = i + 1;
+                    minMonth = j + 1;
+                }
+            }
+        }
+    }
+    printf("Maximo valor: %d\nAnio: %d\nMes: %d\n",maxValue,maxYear,maxMonth);
+    printf("\nMinimo valor: %d\nAnio: %d\nMes: %d\n",minValue,minYear,minMonth);
+}
+
 int main(int arc,char** argv) {
-    unsigned int production[5][12];
+    int production[5][12];
     srand(time(NULL));
     for(int i = 0 ; i < 5 ; i++) {
         for(int j = 0 ; j < 12 ; j++) {
@@ -13,9 +48,11 @@ int main(int arc,char** argv) {
     printf("Valores cargados\n");
     for(int i = 0 ; i < 5 ; i++) {
         for(int j = 0 ; j < 12 ; j++) {
-            printf("%u  ",production[i][j]);
+            printf("%d  ",production[i][j]);
         }
         printf("\r\n");
     }
+    averageRevenue(production);
+    maxAndMin(production);
     return 0;
 }
